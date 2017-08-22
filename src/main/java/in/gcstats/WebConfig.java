2054750a1a393c58398fa0eb002ea0673b0b8935
Sachin Handiekar <sach21@gmail.com>
2017-08-22 09:28:54 +0100

    Update

--- src/main/java/in/gcstats/WebConfig.java
+++ src/main/java/in/gcstats/WebConfig.java
@@ -1,13 +1,28 @@
 package in.gcstats;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
